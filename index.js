2728f7deafc0a907c8b32ad991d84a6a952f136c
Mike Matcke <mr.matcke@gmail.com>
2021-02-18 16:29:41 -0800

    adding questions

--- index.js
+++ index.js
@@ -1,7 +1,94 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//Project name
+const questions = [
+    {
+        type: "input",
+        message:
+        name: "project name"
+    }
+//Project version
+{
+    type: "input",
+    message:
+    name: "version"
+}
+//Project description
+{
+    type: "input",
+    message:
+    name: "description"
+}
+//Project homepage
+{
+    type: "input",
+    message:
+    name: "homepage"
+}
+//Project documentation url
+{
+    type: "input",
+    message:
+    name:"documentation"
+}
+//Project's author's name
+{
+    type: "input",
+    message:
+    name: "author"
+}
+//Project Github username
+{
+    type: "input",
+    message:
+    name: "github"
+}
+//Project Twitter User name
+{
+    type: "input",
+    message:
+    name: "twitter"
+}
+//Project prerquisites
+{
+    type: "input",
+    message:
+    name: "prerquisites"
+}
+//Project license name
+{
+    type: "input",
+    message:
+    name: "licenseName"
+}
+//Project license url
+{
+    type: "input",
+    message:
+    name: "licenseUrl"
+}
+//Project issue page url
+{
+    type: "input",
+    message:
+    name: "issue"
+}
+//Project install commands or instructions
+{
+    type: "input",
+    message:
+    name: "install"
+}
+//Project test command
+{
+    type: "input",
+    message:
+    name: "test"
+}
+
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
