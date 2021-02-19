f58178f07396b2bbce404adb05359d4721d80591
Mike Matcke <mr.matcke@gmail.com>
2021-02-18 22:08:13 -0800

    tried but couldn't get it to work

--- index.js
+++ index.js
@@ -1,89 +1,93 @@
 // TODO: Include packages needed for this application
+const fs = require
+const axios = require("axios");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/genrateMarkdown");
 
 // TODO: Create an array of questions for user input
 //Project name
 const questions = [
     {
         type: "input",
-        message:
-        name: "project name"
+        message: "What is your project's name?"
+        name: "project"
     }
 //Project version
 {
     type: "input",
-    message:
+    message: "What is the version?"
     name: "version"
 }
 //Project description
 {
     type: "input",
-    message:
+    message: "Type in description of project."
     name: "description"
 }
 //Project homepage
 {
     type: "input",
-    message:
+    message: "Enter project's homepage or skip"
     name: "homepage"
 }
 //Project documentation url
 {
     type: "input",
-    message:
+    message: "Please provide documentation url (optional)"
     name:"documentation"
 }
 //Project's author's name
 {
     type: "input",
-    message:
+    message: "Enter the author's name."
     name: "author"
 }
 //Project Github username
 {
     type: "input",
-    message:
+    message: "Enter the github."
     name: "github"
 }
 //Project Twitter User name
 {
     type: "input",
-    message:
+    message: "Please include twitter handle. (optional)"
     name: "twitter"
 }
 //Project prerquisites
 {
     type: "input",
-    message:
+    message: "Enter in any prerequisites"
     name: "prerquisites"
 }
 //Project license name
 {
     type: "input",
-    message:
+    message: "Add in license name (optional)"
     name: "licenseName"
 }
 //Project license url
 {
     type: "input",
-    message:
+    message: "Include License URL (optional)"
     name: "licenseUrl"
 }
 //Project issue page url
 {
     type: "input",
-    message:
+    message: "Include project's issue page url. (optional)"
     name: "issue"
 }
 //Project install commands or instructions
 {
     type: "input",
-    message:
+    message: "Type in any instructions (optional)"
     name: "install"
 }
 //Project test command
 {
     type: "input",
-    message:
+    message: "Type in test command (optional)"
     name: "test"
 }
 
@@ -91,10 +95,24 @@ const questions = [
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//readme was created
+function writeToFile(fileName, data) {
+    fs.writeFile(filename, data, (err)=> {
+        if (err) {
+        throw err;
+    }
+        console.log("Read me was created")
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const response = generatedMarkdown(answers);
+        console.log(answers);
+        writeToFile("ReadMe.md", response);
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,95 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//function renderLicenseBadge(license) {
+  //if (strLicense=== "") {
+    //return (!strLicense)
+  //} 
+  
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+String.prototype.isEmpty = function() {
+  return (this.length === 0 || !this.trim());
+};
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${response.Title} 
 
 `;
 }
+
+
+
+//section lay out.
+
+## Table of Contents
+*[project](#project)
+*[version](#version)
+*[description](#description)
+*[homepage](#homepage)
+*[documentation](#documentation)
+*[author](#author)
+*[github](#github)
+*[twitter](#twitter)
+*[prerquisites](#prequisites)
+*[licenseName](#licenseName)
+*[licenseUrl](#licenseUrl)
+*[issue](#issue)
+*[install](#install)
+*[test](#test)
+
+## 
+## project
+${response.project}
+
+## version
+${response.version}
+
+## description
+${response.description}
+
+## homepage
+${response.homepage}
+
+## documentation
+${response.documentation}
+
+## author
+${response.author}
+
+## github
+${response.github}
+
+## twitter
+${response.twitter}
+
+## prequisites
+${response.prequisites}
+
+## licenseName
+${response.licenseName}
+
+## licenseUrl
+${response.licenseUrl}
+
+## issue
+${response.issue}
+
+## install
+${response.install}
+
+## test
+${response.test}
+
+`;}
+
 
 module.exports = generateMarkdown;
